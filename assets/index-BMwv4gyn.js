18731314bda825c29c85a24b0bfe4bfd6868c02a
global name <global email>
2025-04-08 15:35:55 +0800

    fix: 修改

--- assets/index-BMwv4gyn.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-XDL_Jtnr.js";function e(){return r.jsx(r.Fragment,{children:"404"})}export{e as default};
--- assets/index-LDkNtrV0.js
+++
@@ -1,7 +0,0 @@
-import{j as l}from"./index-XDL_Jtnr.js";const g="_rols_v9i8y_1",s={rols:g},x=function(o){const{children:e}=o;return l.jsx("div",{className:"row",children:e})},c=function(o){const{children:e,className:j,xs:r,sm:n,md:d,lg:i,xl:t,xxl:h}=o;let m=r?"col-xs-"+r:"",a=n?"col-sm-"+n:"",_=d?"col-md-"+d:"",p=i?"col-lg-"+i:"",u=t?"col-xl-"+t:"",C=h?"col-xxl-"+h:"";return l.jsx(l.Fragment,{children:l.jsx("div",{className:`${j} ${m} ${a} ${_} ${p} ${u} ${C}`,children:e})})};function $(){return l.jsxs(l.Fragment,{children:[l.jsx("h1",{children:"栅格"}),l.jsx("h2",{children:"API"}),l.jsx("code",{children:`
-          <Row>
-              <Col xs={6} xl={6}>
-                  Col
-              </Col>
-          </Row> 
-        `}),l.jsx("h3",{children:"演示"}),l.jsx("p",{children:l.jsx(x,{children:l.jsx(c,{className:s.rols,xs:2,sm:4,md:6,lg:8,xl:12,xxl:12,span:12,children:"Col-12 xs-2 sm-4 md-6"})})}),l.jsx("p",{children:l.jsx(x,{children:l.jsx(c,{className:s.rols,xs:2,xl:4,children:"Col-2 xs-2"})})}),l.jsx("p",{children:l.jsx(x,{children:l.jsx(c,{className:s.rols,xs:4,xl:6,children:"Col-4 xs-2"})})}),l.jsx("p",{children:l.jsx(x,{children:l.jsx(c,{className:s.rols,xs:6,xl:8,children:"Col-6 xs-2"})})})]})}export{$ as default};
